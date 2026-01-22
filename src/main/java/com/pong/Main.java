0dce169f161bde758945863825d3a2f87895860b
Ruan Spinelli <ruanspinelli@gmail.com>
2026-01-21 22:03:28 -0300

    Código da main
    O código da classe main permite renderizar as janelas e criar interfaces graficas

--- src/main/java/com/pong/Main.java
+++ src/main/java/com/pong/Main.java
@@ -1,4 +1,16 @@
 package com.pong;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
