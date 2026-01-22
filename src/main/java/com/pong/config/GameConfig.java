307458455bde06792d6df9a322bc801595bbe96c
Ruan Spinelli <ruanspinelli@gmail.com>
2026-01-21 23:35:08 -0300

    GameConfig
    Classe responsável por guardar informações e valores responsáveis por configurar coisas do jogo

---
+++ src/main/java/com/pong/config/GameConfig.java
@@ -0,0 +1,15 @@
+package com.pong.config;
+
+import javafx.scene.paint.Color;
+
+public final class GameConfig {
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    public static final Color BACKGROUND_COLOR = Color.BLACK;
+
+    private GameConfig() {
+        // impede instância
+    }
+}
