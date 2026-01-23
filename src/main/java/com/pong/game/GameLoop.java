d64247f2e48b22a758e38b63339db0ab30c81cba
Ruan Spinelli <ruanspinelli@gmail.com>
2026-01-22 22:56:35 -0300

    Classe game loop
    Classe responsável por lidar com a renderização e atualização visual do jogo

---
+++ src/main/java/com/pong/game/GameLoop.java
@@ -0,0 +1,21 @@
+package com.pong.game;
+
+import javafx.animation.AnimationTimer;
+
+public class GameLoop extends AnimationTimer {
+
+    @Override
+    public void handle(long l) {
+
+    }
+    private void update() {
+        // lógica do jogo (movimento, colisão, regras)
+    }
+
+    private void render() {
+        // desenhar coisas na tela (por enquanto vazio)
+    }
+
+
+
+}
